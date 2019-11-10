56aad881331e189c0914dce0c5abda81c3408035
marom <maro.muszynski@gmail.com>
2019-11-10 21:06:18 +0000

    return user with an id when a user is created

--- src/main/java/com/marom/restfulwebserivces/user/UserResource.java
+++ src/main/java/com/marom/restfulwebserivces/user/UserResource.java
@@ -1,7 +1,10 @@
 package com.marom.restfulwebserivces.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -24,7 +27,13 @@ public class UserResource {
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
         User savedUser = userServiceDao.save(user);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id")
+                .buildAndExpand(savedUser.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
     }
 }
