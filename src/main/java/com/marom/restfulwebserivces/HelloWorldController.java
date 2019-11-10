0e0c84945d1bcd7b855a54efbc68bde7a9772919
marom <maro.muszynski@gmail.com>
2019-11-10 15:17:58 +0000

    add @PathVariable parameter and pass it to the bean

--- src/main/java/com/marom/restfulwebserivces/HelloWorldController.java
+++ src/main/java/com/marom/restfulwebserivces/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.marom.restfulwebserivces;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,10 @@ public class HelloWorldController {
     @GetMapping("/hello-world")
     public HelloWorldBean helloWorld() {
         return new HelloWorldBean("Hello World!");
+    }
+
+    @GetMapping("/hello-world/{name}")
+    public HelloWorldBean helloWorld(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
