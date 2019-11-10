5570f5618a62c9dc65d658d336a90a2ce2a1b342
marom <maro.muszynski@gmail.com>
2019-11-10 14:49:34 +0000

    add HelloWorldBean and modify GET request method to return a bean

---
+++ src/main/java/com/marom/restfulwebserivces/HelloWorldBean.java
@@ -0,0 +1,21 @@
+package com.marom.restfulwebserivces;
+
+public class HelloWorldBean {
+
+    private String message;
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
