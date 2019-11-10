e16bda79003679cc55ff9f8974907555a31236b3
marom <maro.muszynski@gmail.com>
2019-11-10 14:17:14 +0000

    initial commit

---
+++ src/test/java/com/marom/restfulwebserivces/RestfulWebSerivcesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marom.restfulwebserivces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebSerivcesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
