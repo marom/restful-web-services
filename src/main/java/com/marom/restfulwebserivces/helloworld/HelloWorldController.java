996abeb18b44222b2786bc8ddab6111fea1fb7c9
marom <maro.muszynski@gmail.com>
2019-11-30 09:17:49 +0000

    add message source to be able to use internationalised messages

--- src/main/java/com/marom/restfulwebserivces/helloworld/HelloWorldController.java
+++ src/main/java/com/marom/restfulwebserivces/helloworld/HelloWorldController.java
@@ -1,12 +1,22 @@
 package com.marom.restfulwebserivces.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
 
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping("/hello-world")
     public HelloWorldBean helloWorld() {
