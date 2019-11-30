ac90dee0ea720a09d0990a3ae0813425b3850053
marom <maro.muszynski@gmail.com>
2019-11-30 20:09:18 +0000

    Swagger: configure OpenApi to provide info

---
+++ src/main/java/com/marom/restfulwebserivces/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.marom.restfulwebserivces;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("basicScheme",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
+                .info(new Info().title("Restful Web Services API").version(appVersion).description(
+                        "This is a sample of OpenAPI used in my project.  You can find out more about     Swagger at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/).      For this sample, you can use the api key `special-key` to test the authorization     filters.")
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("springdoc.org").url("http://springdoc.org")));
+    }
+}
