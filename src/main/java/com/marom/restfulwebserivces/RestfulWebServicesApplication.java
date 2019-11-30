94a65f3432c31f393daffc4cc6c8b717a5368c98
marom <maro.muszynski@gmail.com>
2019-11-30 08:59:50 +0000

    define messages source bean

--- src/main/java/com/marom/restfulwebserivces/RestfulWebServicesApplication.java
+++ src/main/java/com/marom/restfulwebserivces/RestfulWebServicesApplication.java
@@ -3,6 +3,7 @@ package com.marom.restfulwebserivces;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
@@ -20,5 +21,12 @@ public class RestfulWebServicesApplication {
         SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
         sessionLocaleResolver.setDefaultLocale(Locale.UK);
         return sessionLocaleResolver;
+    }
+
+    @Bean
+    public ResourceBundleMessageSource resourceBundleMessageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
     }
 }
