c208cfc570dbddf67f58cec0ebe7b0c5821aaf37
marom <maro.muszynski@gmail.com>
2019-12-01 07:41:54 +0000

    Static filtering: add Order class

---
+++ src/main/java/com/marom/restfulwebserivces/shop/Order.java
@@ -0,0 +1,44 @@
+package com.marom.restfulwebserivces.shop;
+
+import java.util.Date;
+
+public class Order {
+
+    private Long id = null;
+    private Long petId = null;
+    private Integer quantity = null;
+    private Date shipDate = null;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getPetId() {
+        return petId;
+    }
+
+    public void setPetId(Long petId) {
+        this.petId = petId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Date getShipDate() {
+        return shipDate;
+    }
+
+    public void setShipDate(Date shipDate) {
+        this.shipDate = shipDate;
+    }
+}
+
