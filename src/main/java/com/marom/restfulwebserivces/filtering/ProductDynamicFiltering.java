8656a1fb70422f090274d16b39ef29d590b9c033
marom <maro.muszynski@gmail.com>
2019-12-01 08:27:04 +0000

    Dynamic filtering: define a controller to get filtered Product data

---
+++ src/main/java/com/marom/restfulwebserivces/filtering/ProductDynamicFiltering.java
@@ -0,0 +1,32 @@
+package com.marom.restfulwebserivces.filtering;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import com.marom.restfulwebserivces.shop.Product;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.criteria.Order;
+import java.util.HashSet;
+import java.util.Set;
+
+@RestController
+public class ProductDynamicFiltering {
+
+    @GetMapping("/getSellingPrice")
+    public Product retrieveProductSellingPrice() {
+
+        Product product = new Product("Shoes", 23.5f, 22.1f);
+
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("sellingPrice");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("ProductFilter", filter);
+        MappingJacksonValue value = new MappingJacksonValue(product);
+
+        value.setFilters(filters);
+
+        return product;
+    }
+}
