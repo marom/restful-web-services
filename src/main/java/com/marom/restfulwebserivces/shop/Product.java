7db7d5be0e99a635477d22ddc95724c3c32f4f4e
marom <maro.muszynski@gmail.com>
2019-12-01 08:26:26 +0000

    Dynamic filtering: create a Product class

---
+++ src/main/java/com/marom/restfulwebserivces/shop/Product.java
@@ -0,0 +1,38 @@
+package com.marom.restfulwebserivces.shop;
+
+public class Product {
+
+    private String name;
+    private float sellingPrice;
+    private float buyingPrice;
+
+    public Product(String name, float sellingPrice, float buyingPrice) {
+        this.name = name;
+        this.sellingPrice = sellingPrice;
+        this.buyingPrice = buyingPrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public void setSellingPrice(float sellingPrice) {
+        this.sellingPrice = sellingPrice;
+    }
+
+    public float getBuyingPrice() {
+        return buyingPrice;
+    }
+
+    public void setBuyingPrice(float buyingPrice) {
+        this.buyingPrice = buyingPrice;
+    }
+}
