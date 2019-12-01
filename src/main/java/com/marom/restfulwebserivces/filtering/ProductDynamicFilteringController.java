ff286f34fc376ef2f283f68c06b26a1695741e06
marom <maro.muszynski@gmail.com>
2019-12-01 08:32:00 +0000

    Dynamic filtering: refactor

--- src/main/java/com/marom/restfulwebserivces/filtering/ProductDynamicFilteringController.java
+++ src/main/java/com/marom/restfulwebserivces/filtering/ProductDynamicFilteringController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ProductDynamicFiltering {
+public class ProductDynamicFilteringController {
 
     @GetMapping("/getSellingPrice")
     public MappingJacksonValue retrieveProductSellingPrice() {
