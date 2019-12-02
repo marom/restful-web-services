9214a205d6e8496693c14fac3da0729c4d358caa
marom <maro.muszynski@gmail.com>
2019-12-02 20:38:50 +0000

    Versioning: add v2 of Person class

---
+++ src/main/java/com/marom/restfulwebserivces/versioning/PersonV2.java
@@ -0,0 +1,13 @@
+package com.marom.restfulwebserivces.versioning;
+
+public class PersonV2 {
+
+    private Name name;
+
+    public PersonV2(Name name) {
+        this.name = name;
+    }
+
+    public PersonV2() {
+    }
+}
