00b76c39723e15ece930c91e9ec7b614d1187e88
marom <maro.muszynski@gmail.com>
2019-12-02 20:49:16 +0000

    Versioning: add simple requests for both versions of Person class

---
+++ src/main/java/com/marom/restfulwebserivces/versioning/PersonVersioningController.java
@@ -0,0 +1,18 @@
+package com.marom.restfulwebserivces.versioning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonVersioningController {
+
+    @GetMapping("/v1/person")
+    public PersonV1 personV1() {
+        return new PersonV1("PersonV1 Name");
+    }
+
+    @GetMapping("/v2/person")
+    public PersonV2 personV2() {
+        return new PersonV2(new Name("PersonV2", "NameV2"));
+    }
+}
