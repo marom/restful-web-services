c43b74deb596d3007605d91dbacd36a48348ab35
marom <maro.muszynski@gmail.com>
2019-12-07 08:43:34 +0000

    JPA: an user is managed by JPA

--- src/main/java/com/marom/restfulwebserivces/user/User.java
+++ src/main/java/com/marom/restfulwebserivces/user/User.java
@@ -2,21 +2,30 @@ package com.marom.restfulwebserivces.user;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Schema(description = "All details about a user.")
+@Entity
 public class User {
 
-
+    @Id
+    @GeneratedValue
     private Integer id;
+
     @Size(min = 2, message = "Name should have at least 2 characters.")
     private String name;
 
     @Schema(description = "Birth date should be in the past.")
     @Past
     private Date birthDate;
+
+    public User() {
+    }
 
     public User(Integer id, String name, Date birthDate) {
         this.id = id;
