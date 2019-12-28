5ee875000eb7050e9879b9f30b6d923439f772bb
marom <maro.muszynski@gmail.com>
2019-12-28 12:29:05 +0000

    JPA: add posts to the database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
 insert into USER values ( 1001, sysdate, 'Mark' );
 insert into USER values ( 1002, sysdate, 'Anna' );
-insert into USER values ( 1003, sysdate, 'Michael' )
+insert into USER values ( 1003, sysdate, 'Michael' );
+
+insert into POST values (10001, 'This is my first post.', 1001);
+insert into POST values (10002, 'This is my second post.', 1001);
